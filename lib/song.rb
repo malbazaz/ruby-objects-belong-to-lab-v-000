ce03f8f52cc1898b6066743f148319a93c6c2955
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-21 19:44:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,9 @@ class Song
 
 attr_accessor :title, :artist   
 
-def initialize
-@title = title 
-@artist = artist 
-end 
+  def initialize
+    @title = title 
+    @artist = artist 
+  end 
   
 end 
